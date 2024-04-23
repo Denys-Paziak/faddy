c6eb77c32bef5030231deaf151a9400298e7e7cd
Denys Paziak <67861412+Denys-Paziak@users.noreply.github.com>
2024-04-24 02:03:34 +0300

    add back

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -22,7 +22,7 @@ const Login = () => {
     const handleSubmit = async (e: any) => {
         e.preventDefault();
         try {
-            const response = await fetch('http://localhost:3002/login', {
+            const response = await fetch('https://famdy-back.vercel.app/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -22,7 +22,7 @@ const Register = () => {
     const handleSubmit = async (e: any) => {
         e.preventDefault();
         try {
-            const response = await fetch('http://localhost:3002/register', {
+            const response = await fetch('https://famdy-back.vercel.app/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
